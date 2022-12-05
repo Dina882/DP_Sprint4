4b481e700c1f30de6b4f062edc54d79fc61197f6
Dina Ponomareva <dina882@yandex.ru>
2022-12-05 21:36:03 +0500

    Push

--- src/test/java/order/MakeOrder.java
+++ src/test/java/order/MakeOrder.java
@@ -1,2 +1,87 @@
-package order;public class MakeOrder {
+package order;
+
+import PageObject.OrderElements;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+
+
+
+
+public class MakeOrder {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+//      выбораем браузер
+        driver = new ChromeDriver();
+//      driver = new FirefoxDriver();
+//      для проверки браузера Firefox
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+
+    @Test
+    public void checkUpOrder() {
+//      проверка кнопки "Заказать" вверху страницы
+        OrderElements orderElements = new OrderElements(driver);
+        orderElements.open();
+        orderElements.clickCookie(); // закрываем куки
+        orderElements.clickButtonUpOrder(); // нажимаем кнопку заказать
+        orderElements.IsOrderPageOpen();
+        orderElements.sendName();
+        orderElements.sendSurnameName();
+        orderElements.sendAdress();
+        orderElements.clickMetro();
+        orderElements.clickMetro();
+        orderElements.sendPhoneNumber();
+        orderElements.clickButtonNext();
+        orderElements.IsSecondOrderPageOpen();
+        orderElements.clickWhenBringOrder();
+        orderElements.choiceWhenBringOrder();
+        orderElements.choiceDateRent();
+        orderElements.choiceColor();
+        orderElements.comment();
+        orderElements.clickToOrder();
+        orderElements.clickYes();
+        orderElements.IsThirdOrderPageOpen();
+    }
+
+
+    @Test
+    public void checkDownOrder() {
+//      проверка кнопки "Заказать" внизу страницы
+        OrderElements orderElements = new OrderElements(driver);
+        orderElements.open();
+        orderElements.clickCookie(); // закрываем куки
+        orderElements.clickButtonDownOrder(); // скролив вниз и нажимаем кнопку заказать
+        orderElements.IsOrderPageOpen();
+        orderElements.sendName();
+        orderElements.sendSurnameName();
+        orderElements.sendAdress();
+        orderElements.clickMetro();
+        orderElements.clickMetro();
+        orderElements.sendPhoneNumber();
+        orderElements.clickButtonNext();
+        orderElements.IsSecondOrderPageOpen();
+        orderElements.clickWhenBringOrder();
+        orderElements.choiceWhenBringOrder();
+        orderElements.choiceDateRent();
+        orderElements.choiceColor();
+        orderElements.comment();
+        orderElements.clickToOrder();
+        orderElements.clickYes();
+        orderElements.IsThirdOrderPageOpen();
+    }
+    @After
+    public void teardown() {
+//      Закрываем браузер
+        driver.quit();
+    }
 }
--- src/test/java/questions/ClickDropdown.java
+++ src/test/java/questions/ClickDropdown.java
@@ -1,2 +1,55 @@
-package questions;public class ClickDropdown {
+package questions;
+
+import PageObject.HomePage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+
+public class ClickDropdown {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+//      выбораем браузер
+        driver = new ChromeDriver();
+//      driver = new FirefoxDriver();
+//      для проверки браузера Firefox
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @Test
+    public void checkAllText() {
+        HomePage mainPage = new HomePage(driver);
+        mainPage.open(); // открытие ссылки
+        mainPage.clickCookie(); // закрываем куки
+        mainPage.scrollHowMuch(); // скролл
+        mainPage.clickHowMuch(); // кликаем вопрос "Сколько это стоит? И как оплатить?"
+        mainPage.IsAnswerHowMuch();
+        mainPage.clickWantSeveralScooters(); //кликаем вопрос "Хочу сразу несколько самокатов! Так можно?"
+        mainPage.IsAnswerSeveralScooters();
+        mainPage.clickHowRentalTimeCalculated(); //кликаем вопрос "Как рассчитывается время аренды?"
+        mainPage.IsAnswerHowRentalTimeCalculated();
+        mainPage.clickTodayOrder(); //кликаем вопрос "Можно ли заказать самокат прямо на сегодня?"
+        mainPage.IsAnswerTodayOrder();
+        mainPage.clickExtendOrder(); //кликаем вопрос "Можно ли продлить заказ или вернуть самокат раньше?"
+        mainPage.IsAnswerExtendOrder();
+        mainPage.clickCharging(); //кликаем вопрос "Вы привозите зарядку вместе с самокатом?"
+        mainPage.IsAnswerCharging();
+        mainPage.clickCanOrderCancellation(); //кликаем вопрос "Можно ли отменить заказ?"
+        mainPage.IsAnswerCanOrderCancellation();
+        mainPage.clickLiveOutsideMoscow(); //кликаем вопрос "Я жизу за МКАДом, привезёте?"
+        mainPage.IsAnswerLiveOutsideMoscow();
+    }
+    @After
+    public void teardown() {
+//      Закрываем браузер
+        driver.quit();
+    }
+
 }
+
