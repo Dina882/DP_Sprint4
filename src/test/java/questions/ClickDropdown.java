9e6f50aa7f7f38765038a4a11f9b0b5346f39666
Dina <115890831+Dina882@users.noreply.github.com>
2022-12-07 00:32:06 +0500

    Update ClickDropdown.java

--- src/test/java/questions/ClickDropdown.java
+++ src/test/java/questions/ClickDropdown.java
@@ -18,7 +18,7 @@ public class ClickDropdown {
 //      выбираем браузер
         driver = new ChromeDriver();
 //      driver = new FirefoxDriver();
-//      для проверки браузера Firefox
+//        для проверки браузера Firefox
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
