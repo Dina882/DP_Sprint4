7416abba46a43699715512d87107a37cfec13fc8
Dina <115890831+Dina882@users.noreply.github.com>
2022-12-07 00:30:30 +0500

    Update ClickDropdown.java

--- src/test/java/questions/ClickDropdown.java
+++ src/test/java/questions/ClickDropdown.java
@@ -15,7 +15,7 @@ public class ClickDropdown {
 
     @Before
     public void setUp() {
-//     выбираем браузер
+//      выбираем браузер
         driver = new ChromeDriver();
 //      driver = new FirefoxDriver();
 //      для проверки браузера Firefox
